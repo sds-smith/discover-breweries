0ecfa050795ebf881855fbf260195774561a088d
Shawn Smith <sds.smith24@gmail.com>
2023-01-12 09:23:25 -0500

    clean up unused imports

--- client/src/routes/breweries-near-me/breweries-near-me.component.tsx
+++ client/src/routes/breweries-near-me/breweries-near-me.component.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
 
 import CityTable from "../../components/city-table/city-table.component";
 
@@ -6,7 +6,6 @@ import useGetBreweries from "../../utils/hooks/use-get-breweries";
 
 import { BreweryContext } from "../../context/brewery.context";
 import { ClientContext } from "../../context/client.context";
-import { BreweryArray } from "../../utils/types.utils";
 
 const BreweriesNearMe = () => {
 

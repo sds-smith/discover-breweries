e3e14c9da6f352b9f7780bcfec77da99e6cf25b3
Shawn Smith <sds.smith24@gmail.com>
2023-09-08 09:51:24 -0400

    delete server

--- server/src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const breweriesRouter = require('./breweries/breweries.router')
-
-const api = express.Router();
-
-api.use('/breweries', breweriesRouter)
-
-module.exports = api;
